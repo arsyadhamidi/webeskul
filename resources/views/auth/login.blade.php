e25d5ffcdb0e014558d5f0aad550102d22f81427
Arsyad Hamidi <arsyadhamidi04@gmail.com>
2024-09-12 08:57:51 +0700

    commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Login | Webeskul </title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/feather/feather.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/mdi/css/materialdesignicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/ti-icons/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/font-awesome/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/typicons/typicons.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/simple-line-icons/css/simple-line-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/css/vendor.bundle.base.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css') }}">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" />
+</head>
+
+<body>
+    <div class="container-scroller">
+        <div class="container-fluid page-body-wrapper full-page-wrapper">
+            <div class="content-wrapper d-flex align-items-center auth px-0">
+                <div class="row w-100 mx-0">
+                    <div class="col-lg-4 mx-auto">
+                        <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+                            <div class="brand-logo">
+                                <img src="{{ asset('images/logo.png') }}" alt="logo">
+                            </div>
+                            <h4>Hello! Ayo segerakan untuk memulai</h4>
+                            <h6 class="fw-light">Login untuk melanjutkan.</h6>
+                            <form class="pt-3">
+                                <div class="form-group">
+                                    <input type="text" name="username"
+                                        class="form-control form-control-lg @error('username') is-invalid @enderror"
+                                        placeholder="Username" value="{{ old('username') }}">
+                                    @error('username')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <input type="password"
+                                        class="form-control form-control-lg @error('password') is-invalid @enderror"
+                                        placeholder="Password">
+                                    @error('password')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                                </div>
+                                <div class="mt-3 d-grid gap-2">
+                                    <button type="submit" class="btn btn-block btn-primary btn-lg fw-medium auth-form-btn">MASUK</button>
+                                </div>
+                                <div class="my-2 d-flex justify-content-between align-items-center">
+                                    <div class="form-check">
+                                        <label class="form-check-label text-muted">
+                                            <input type="checkbox" class="form-check-input"> Keep me signed in </label>
+                                    </div>
+                                    <a href="#" class="auth-link text-black">Lupa password?</a>
+                                </div>
+                                <div class="text-center mt-4 fw-light"> Anda tidak punya akun ? <a href="#"
+                                        class="text-primary">Register!</a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- content-wrapper ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="{{ asset('admin/assets/vendors/js/vendor.bundle.base.js') }}"></script>
+    <script src="{{ asset('admin/assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js') }}"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="{{ asset('admin/assets/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/template.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/settings.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/todolist.js') }}"></script>
+    <!-- endinject -->
+</body>
+
+</html>
