6a504a8ca3ab62b846fb935c879e2fa12d7f7c5d
Arsyad Hamidi <arsyadhamidi04@gmail.com>
2024-09-19 13:56:50 +0700

    commit

--- database/migrations/2024_09_19_063925_create_pendaftarans_table.php
+++ database/migrations/2024_09_19_063925_create_pendaftarans_table.php
@@ -13,6 +13,21 @@ return new class extends Migration
     {
         Schema::create('pendaftarans', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('eskul_id');
+            $table->foreignId('siswa_id');
+            $table->foreignId('jurusan_id');
+            $table->foreignId('kelas_id');
+            $table->string('nis');
+            $table->string('nama');
+            $table->string('tmp_lahir');
+            $table->string('tgl_lahir');
+            $table->string('jk');
+            $table->string('telp');
+            $table->string('email');
+            $table->string('status');
+            $table->text('alasan');
+            $table->text('alamat');
+            $table->string('berkas_pendaftaran');
             $table->timestamps();
         });
     }
--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -144,7 +144,7 @@
                             <li class="nav-item"> <a class="nav-link" href="{{ route('data-pembina.index') }}">Data Pembina</a></li>
                             <li class="nav-item"> <a class="nav-link" href="{{ route('data-ortu.index') }}">Data Orang Tua</a></li>
                             <li class="nav-item"> <a class="nav-link" href="{{ route('data-siswa.index') }}">Data Siswa</a></li>
-                            <li class="nav-item"> <a class="nav-link" href="{{ route('data-eskul.index') }}">Data Eskul</a></li>
+                            <li class="nav-item"> <a class="nav-link" href="{{ route('data-eskul.index') }}">Data Ekskul</a></li>
                           </ul>
                         </div>
                       </li>
